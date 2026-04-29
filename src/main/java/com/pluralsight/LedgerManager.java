aa072442d5cafc27ec1468ed0bb0c8638cad122b
kay8181 <you@example.com>
2026-04-29 11:46:32 -0700

    csv file loading and updated display and report menu

--- src/main/java/com/pluralsight/LedgerManager.java
+++ src/main/java/com/pluralsight/LedgerManager.java
@@ -1,6 +1,10 @@
 package com.pluralsight;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDate;
@@ -9,7 +13,7 @@ import java.time.LocalTime;
 public class LedgerManager {
     private ArrayList<Transaction> transactions;
     public LedgerManager() {
-        transactions = new ArrayList<>();
+        transactions = loadTransactions("transactions.csv");
     }
     public void ledgerMenu(Scanner scanner) {
         String userInput = "";
@@ -20,9 +24,7 @@ public class LedgerManager {
                 case "A":
                     System.out.println("All:");
                     allTransactions();
-
                     break;
-
                 case "D":
                     System.out.println("Deposits:");
 
@@ -59,10 +61,10 @@ public class LedgerManager {
             System.out.println("       Reports/Search      ");
             System.out.println("===========================");
             //System.out.println("What would you like to do?");
-            System.out.println("1. Month To Date\n2. Previous Month\n3. Year To Date\n" +
-                    "4. Previous Year\n5. Search by Vendor\n6. Custom Search\n0. Back\nH. Home");
+            System.out.println("1. Month to Date\n2. Previous Month\n3. Year to Date\n" +
+                    "4. Previous Year\n5. Search by Vendor\n6. Custom Search\n0. Back");
     }
-    public void customSearch() {
+    public void customSearchDisplay() {
         System.out.println("===========================");
         System.out.println("           Search          ");
         System.out.println("===========================");
@@ -78,7 +80,6 @@ public class LedgerManager {
             System.out.println("Enter deposit amount:");
             amount=scanner.nextDouble();
         }
-
         System.out.println("Enter the vendor");
         scanner.nextLine();
         String vendor= scanner.nextLine();
@@ -113,10 +114,102 @@ public class LedgerManager {
 
     }
     public void allTransactions() {
+        DateTimeFormatter fmtDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter fmtTime = DateTimeFormatter.ofPattern("HH:mm:ss");
         for(Transaction t: transactions) {
-            System.out.println(t.getAmount() + t.getDescription());
+            String formattedDate = t.getDate().format(fmtDate);
+            String formattedTime = t.getTime().format(fmtTime);
+            System.out.printf("%s | %s | %s | %s | $%.2f\n", formattedDate, formattedTime, t.getDescription(),
+                   t.getVendor(), t.getAmount());
         }
 
     }
+    public void reportsMenu(Scanner scanner) {
+        int userInput=-1;
+        while(userInput!=0) {
+           reportsDisplay();
+            userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+                    System.out.println("Month to Date:");
+                    monthToDate();
+                    break;
+                case 2:
+                    System.out.println("Previous Month:");
+                    previousMonth();
+                    break;
+                case 3:
+                    System.out.println("Year to Date:");
+                    yearToDate();
+                    break;
+                case 4:
+                    System.out.println("Previous Year:");
+                    previousYear();
+                    break;
+                case 5:
+                    System.out.println("Search by Vendor:");
+                    searchByVendor();
+                    break;
+                case 6:
+                    System.out.println("Custom Search:");
+                    customSearch();
+                    break;
+                case 0:
+                    System.out.println("Back to ledger page");
+                    return;
+                default:
+                    System.out.println("Invalid option. Try again...");
+            }
 
+        }
+    }
+    public void monthToDate() {
+
+    }
+    public void previousMonth() {
+
+    }
+    public void yearToDate() {
+
+    }
+    public void previousYear() {
+
+    }
+    public void searchByVendor(){
+
+    }
+    public void customSearch() {
+
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+    public ArrayList<Transaction> loadTransactions(String fileName) {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String line;
+            bufReader.readLine();
+            while ((line = bufReader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                System.out.println(line);
+                if (parts.length == 5) {
+                    LocalDate date = LocalDate.parse(parts[0].trim());
+                    LocalTime time= LocalTime.parse(parts[1].trim());
+                    String description = parts[2].trim();
+                    String vendor = parts[3].trim();
+                    double amount = Double.parseDouble(parts [4].trim());
+
+                    transactions.add(new Transaction(date, time, description, vendor, amount));
+                }
+            }
+            bufReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return transactions;
+    }
 }
